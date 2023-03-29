4fa17e6fd92ed3558806519414310fad19780262
Shiju Kalluvilayil Babu(UST <227588@ust-global.com>
2023-03-29 20:27:20 +0530

    Initial Commit

---
+++ src/main/java/com/ust/sourcecourse/configuration/SourceCourseConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.ust.sourcecourse.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SourceCourseConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SourceCourseConfigurationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ust/sourcecourse/configuration/SourceCourseConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ust.sourcecourse.configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SourceCourseConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
