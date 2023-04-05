330bb16e607929dffd43ce114e9b7ea22ac34780
Shiju K Babu <shiju.kbabu@ust.com>
2023-04-05 20:47:48 +0530

    Correction

--- src/main/java/com/ust/sourcecourse/configuration/service/DBDataSourceService.java
+++ src/main/java/com/ust/sourcecourse/configuration/service/DBDataSourceService.java
@@ -15,9 +15,6 @@ public class DBDataSourceService {
 	@Autowired
 	private DataSourceRepository dataSourceRepository;
 
-//	@Autowired
-//	private ConnectionInfoRepository connectionInfoRepository;
-
 	public DBDataSourceInfo saveDB(DBInfo dbInfo) {
 		ConnectionInfo connectionInfo = ConnectionInfo.builder().connectionURL(dbInfo.getConnectionURL())
 				.username(dbInfo.getUsername()).password(dbInfo.getPassword()).build();
@@ -25,7 +22,6 @@ public class DBDataSourceService {
 				.connectionInfo(connectionInfo).build();
 		connectionInfo.setDataSource(dataSource);
 		dataSource = dataSourceRepository.save(dataSource);
-//		connectionInfo = connectionInfoRepository.save(connectionInfo);
 		return DBDataSourceInfo.builder().uid(dataSource.getUid()).name(dataSource.getName())
 				.description(dataSource.getDescription()).build();
 	}
