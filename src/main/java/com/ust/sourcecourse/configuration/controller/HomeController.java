20b42d31627ce297ce0db66b9945822af28c6ddc
Chandini Bodigadla(UST <212634@ust-global.com>
2023-04-12 14:59:39 +0530

    Home page

--- src/main/java/com/ust/sourcecourse/configuration/controller/HomeController.java
+++ src/main/java/com/ust/sourcecourse/configuration/controller/HomeController.java
@@ -5,29 +5,22 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ust.sourcecourse.configuration.entity.Project;
-import com.ust.sourcecourse.configuration.service.ProjectService;
+import com.ust.sourcecourse.configuration.response.HomePageResponse;
+import com.ust.sourcecourse.configuration.service.HomeService;
 
 @RestController
-@RequestMapping("/projects")
+@RequestMapping("/home")
 public class HomeController {
-	@GetMapping("/projects/{id}")
-	public Project getProjectById(@PathVariable Long id) {
-		return projectService.getProjectById(id);
+
+	@Autowired
+	private HomeService homeService;
+
+	@GetMapping
+	public ResponseEntity<List<HomePageResponse>> getHomePageDetails() {
+		return ResponseEntity.ok(homeService.getHomePageDetails());
 	}
-	@GetMapping("/projects")
-	public List<Project> getAllProjects() {
-		return projectService.getAllProjects();
-	}
-	}
 
-	
-
-
-
-
-
+}
--- src/main/java/com/ust/sourcecourse/configuration/service/HomeService.java
+++ src/main/java/com/ust/sourcecourse/configuration/service/HomeService.java
@@ -1,6 +1,6 @@
 package com.ust.sourcecourse.configuration.service;
 
-import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,43 +8,51 @@ import org.springframework.stereotype.Service;
 
 import com.ust.sourcecourse.configuration.entity.Project;
 import com.ust.sourcecourse.configuration.repository.ProjectRepository;
+import com.ust.sourcecourse.configuration.response.HomePageResponse;
 
-import jakarta.persistence.EntityNotFoundException;
-
+@Service
 public class HomeService {
-	@Service
-	public class ProjectService {
-		
+	
+
 		@Autowired
 		private ProjectRepository projectRepository;
 
-		public Project createProject(Project project) {
-			return projectRepository.save(project);
-		}
-		
-		public Project getProjectById(Long id) {
-			return projectRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Project not found with id: " + id));
+		public List<HomePageResponse> getHomePageDetails() {
+			List<Project> projects = projectRepository.findAll();
+			List<HomePageResponse> homePageResponses = new ArrayList<>();
+			for (Project project : projects) {
+				HomePageResponse response = HomePageResponse.builder().projectName(project.getName())
+						.description(project.getDescription()).build();
+				response.setTables(project.getProjectTables().size());
+				response.setGroups(project.getGroups().size());
+				homePageResponses.add(response);
+			}
+			return homePageResponses;
 		}
 
-		public List<Project> getAllProjects() {
-			return projectRepository.findAll();
-		}
-		
-		public Project updateProject(Long id, Project project) {
-			Project existingProject = projectRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Project not found with id: " + id));
-			existingProject.setName(project.getName());
-			existingProject.setDescription(project.getDescription());
-			existingProject.setModifiedBy(project.getModifiedBy());
-			existingProject.setModifiedTimestamp(LocalDateTime.now());
-			return projectRepository.save(existingProject);
-		}
-		
-		public void deleteProject(Long id) {
-			projectRepository.deleteById(id);
-		}
-		
-	}
+//		public Project createProject(Project project) {
+//			return projectRepository.save(project);
+//		}
+//		
+//		public Project getProjectById(Long id) {
+//			return projectRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Project not found with id: " + id));
+//		}
+//
+//		public List<Project> getAllProjects() {
+//			return projectRepository.findAll();
+//		}
+//		
+//		public Project updateProject(Long id, Project project) {
+//			Project existingProject = projectRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Project not found with id: " + id));
+//			existingProject.setName(project.getName());
+//			existingProject.setDescription(project.getDescription());
+//			existingProject.setModifiedBy(project.getModifiedBy());
+//			existingProject.setModifiedTimestamp(LocalDateTime.now());
+//			return projectRepository.save(existingProject);
+//		}
+//		
+//		public void deleteProject(Long id) {
+//			projectRepository.deleteById(id);
+//		}
 
 }
-
-	
