300025622fa59690fb2d79d7700ae048bb1ea11c
Shiju K Babu <shiju.kbabu@ust.com>
2023-04-25 17:47:19 +0530

    API

--- src/main/java/com/ust/sourcecourse/configuration/service/ColumnsService.java
+++ src/main/java/com/ust/sourcecourse/configuration/service/ColumnsService.java
@@ -1,7 +1,6 @@
 
 package com.ust.sourcecourse.configuration.service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -30,14 +29,15 @@ public class ColumnsService {
 
 	@Autowired
 	private ProjectGroupRepository projectGroupRepository;
+
 	@Autowired
 	private GroupColumnRepository groupColumnRepository;
+
 	public List<ColumnsResponse> getColumnInfo(Long groupId) {
 		Optional<ProjectGroup> optional = projectGroupRepository.findById(groupId);
 		if (optional.isPresent()) {
 			ProjectGroup projectGroup = optional.get();
-			return getColumnResponses(projectGroup);
-
+			return getColumnResponses(projectGroup.getGroupColumns());
 		} else {
 			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Project Group with uid " + groupId + " not found");
 		}
@@ -46,40 +46,31 @@ public class ColumnsService {
 
 	@Transactional
 	public List<ColumnsResponse> saveData(Long groupId, List<ColumnsRequest> columnsRequests) {
-		Optional<ProjectGroup> optional = projectGroupRepository.findById(groupId);
-
-		if (optional.isPresent()) {
-			ProjectGroup projectGroup = optional.get();
-			List<GroupColumn> groupColumns = projectGroup.getGroupColumns();
-			if (groupColumns == null) {
-				groupColumns = new ArrayList<>();
-			}
-
-			for (ColumnsRequest columnsRequest : columnsRequests) {
+		ProjectGroup projectGroup = projectGroupRepository.findById(groupId)
+				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+						"Project Group with uid " + groupId + " not found"));
+		if (columnsRequests != null && !columnsRequests.isEmpty()) {
+			List<GroupColumn> groupColumns = columnsRequests.stream().map(columnsRequest -> {
 				SourceColumn sourceColumn = columnsRepository.findById(columnsRequest.getSourceColumnUid())
-						.orElseThrow();
+						.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+								"Source Column with uid " + columnsRequest.getSourceColumnUid() + " not found"));
+				return GroupColumn.builder().name(columnsRequest.getName()).notes(columnsRequest.getNotes())
+						.type(columnsRequest.getType()).isPrimary(columnsRequest.isPrimary())
+						.defaultValue(columnsRequest.getDefaultvalue()).prefix(columnsRequest.getPreffix())
+						.suffix(columnsRequest.getSuffix()).projectGroup(projectGroup).sourceColumn(sourceColumn)
+						.build();
+			}).toList();
 
-				GroupColumn groupColumn = GroupColumn.builder().name(columnsRequest.getName())
-						.notes(columnsRequest.getNotes()).type(columnsRequest.getType())
-						.isPrimary(columnsRequest.isPrimary()).defaultValue(columnsRequest.getDefaultvalue())
-						.prefix(columnsRequest.getPreffix()).suffix(columnsRequest.getSuffix())
-						.projectGroup(projectGroup).sourceColumn(sourceColumn).build();
-
-				groupColumns.add(groupColumn);
-			}
-			projectGroup.setGroupColumns(groupColumns);
-			projectGroup = projectGroupRepository.save(projectGroup);
-			return getColumnResponses(projectGroup);
+			groupColumns = groupColumnRepository.saveAll(groupColumns);
+			return getColumnResponses(groupColumns);
 
 		} else {
-			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Project Group with uid " + groupId + " not found");
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid Column requests");
 		}
-
 	}
 
-	private List<ColumnsResponse> getColumnResponses(ProjectGroup projectGroup) {
-		List<ColumnsResponse> columnsResponses = new ArrayList<>();
-		for (GroupColumn groupColumn : projectGroup.getGroupColumns()) {
+	private List<ColumnsResponse> getColumnResponses(List<GroupColumn> groupColumns) {
+		List<ColumnsResponse> columnsResponses = groupColumns.stream().map(groupColumn -> {
 			AttributesInfo attributesInfo = new AttributesInfo();
 			attributesInfo.setName(groupColumn.getName());
 			attributesInfo.setNotes(groupColumn.getNotes());
@@ -90,9 +81,8 @@ public class ColumnsService {
 			attributesInfo.setSuffix(groupColumn.getSuffix());
 			SourceInfo sourceInfo = new SourceInfo(groupColumn.getSourceColumn().getName(),
 					groupColumn.getSourceColumn().getUid());
-			ColumnsResponse columnsResponse = new ColumnsResponse(groupColumn.getUid(), attributesInfo, sourceInfo);
-			columnsResponses.add(columnsResponse);
-		}
+			return new ColumnsResponse(groupColumn.getUid(), attributesInfo, sourceInfo);
+		}).toList();
 		return columnsResponses;
 	}
 
@@ -102,50 +92,42 @@ public class ColumnsService {
 			GroupColumn groupColumn = optional.get();
 			groupColumn.setName(columnsRequest.getName());
 			groupColumn.setNotes(columnsRequest.getNotes());
-        	groupColumn.setType(columnsRequest.getType());
-        	groupColumn.setPrimary(columnsRequest.isPrimary());
-        	groupColumn.setDefaultValue(columnsRequest.getDefaultvalue());
-        	groupColumn.setSuffix(columnsRequest.getSuffix());
-        	groupColumn.setPrefix(columnsRequest.getPreffix());
-            if(groupColumn.getUid().equals(columnId)) {
-            	SourceColumn sourceColumn = columnsRepository.findById(columnsRequest.getSourceColumnUid())
-            			.orElseThrow();
-            	groupColumn.setSourceColumn(sourceColumn);
-            	
-            }
-            groupColumnRepository.save(groupColumn);
-	        	AttributesInfo attributesInfo = new AttributesInfo();
-	        	attributesInfo.setName(groupColumn.getName());
-	        	attributesInfo.setNotes(groupColumn.getNotes());
-	        	attributesInfo.setType(groupColumn.getType());
-	        	attributesInfo.setPrimary(groupColumn.isPrimary());
-	        	attributesInfo.setDefaultValue(groupColumn.getDefaultValue());
-	        	attributesInfo.setPrefix(groupColumn.getPrefix());
-	        	attributesInfo.setSuffix(groupColumn.getSuffix());
-	        	SourceInfo SourceInfo = new SourceInfo(groupColumn.getSourceColumn().getName(),groupColumn.getSourceColumn().getUid());
-	        	
-	        	ColumnsResponse columnsResponse = new ColumnsResponse(groupColumn.getUid(),attributesInfo,SourceInfo);
-	        	
-	        	return columnsResponse;
-	        }else {
-	    		throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Group column with uid " + columnId + " not found");
-	    		
-	    	}	
-	
-}
+			groupColumn.setType(columnsRequest.getType());
+			groupColumn.setPrimary(columnsRequest.isPrimary());
+			groupColumn.setDefaultValue(columnsRequest.getDefaultvalue());
+			groupColumn.setSuffix(columnsRequest.getSuffix());
+			groupColumn.setPrefix(columnsRequest.getPreffix());
+			if (groupColumn.getUid().equals(columnId)) {
+				SourceColumn sourceColumn = columnsRepository.findById(columnsRequest.getSourceColumnUid())
+						.orElseThrow();
+				groupColumn.setSourceColumn(sourceColumn);
+
+			}
+			groupColumnRepository.save(groupColumn);
+			AttributesInfo attributesInfo = new AttributesInfo();
+			attributesInfo.setName(groupColumn.getName());
+			attributesInfo.setNotes(groupColumn.getNotes());
+			attributesInfo.setType(groupColumn.getType());
+			attributesInfo.setPrimary(groupColumn.isPrimary());
+			attributesInfo.setDefaultValue(groupColumn.getDefaultValue());
+			attributesInfo.setPrefix(groupColumn.getPrefix());
+			attributesInfo.setSuffix(groupColumn.getSuffix());
+			SourceInfo SourceInfo = new SourceInfo(groupColumn.getSourceColumn().getName(),
+					groupColumn.getSourceColumn().getUid());
+
+			ColumnsResponse columnsResponse = new ColumnsResponse(groupColumn.getUid(), attributesInfo, SourceInfo);
+
+			return columnsResponse;
+		} else {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Group column with uid " + columnId + " not found");
+
+		}
+
+	}
 
 	public void deleteData(Long columnId) {
 		groupColumnRepository.deleteById(columnId);
-		
-		}
-		
+
 	}
-		// TODO Auto-generated method stub
-		
-	
-	    
-	
 
-	
-	
-
+}
