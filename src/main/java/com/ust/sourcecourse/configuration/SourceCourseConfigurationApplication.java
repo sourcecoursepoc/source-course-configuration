8c1bb0a2c18e22e86d50dc7d06f5bb573211a0f4
Shiju K Babu <shiju.kbabu@ust.com>
2023-05-26 15:23:38 +0530

    Kafka

--- src/main/java/com/ust/sourcecourse/configuration/SourceCourseConfigurationApplication.java
+++ src/main/java/com/ust/sourcecourse/configuration/SourceCourseConfigurationApplication.java
@@ -2,8 +2,10 @@ package com.ust.sourcecourse.configuration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class SourceCourseConfigurationApplication {
 
 	public static void main(String[] args) {
